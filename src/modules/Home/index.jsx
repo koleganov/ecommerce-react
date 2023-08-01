0142280ae8ab48f608e8a40ce6a1de4fae7f8bd1
koleganov <taggz1030@gmail.com>
2023-08-01 17:45:36 +0500

    added components and fetch request in fake api store

---
+++ src/modules/Home/index.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import Hero from "../../components/Hero";
+import Products from "../../components/Products";
+import FeatureCard from "../../components/FeatureCard";
+import Stats from "../../components/StatCard";
+
+const Home = () => {
+  const [products, setProducts] = React.useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <Hero />
+      <div className="flex flex-col text-center w-full mb-20">
+        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+          Products
+        </h2>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+          MOST POPULAR PRODUCTS
+        </h1>
+      </div>
+      {products.length > 0 ? (
+        <Products products={products} />
+      ) : (
+        <div>Loading....</div>
+      )}
+      <FeatureCard />
+      <Stats />
+    </>
+  );
+};
+
+export default Home;
